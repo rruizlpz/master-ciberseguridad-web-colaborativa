889f9492d127273082503e4c442d1ada7eed1174
Javi Espinosa <javier.espinosa@11paths.com>
2021-12-15 19:01:38 +0100

    fix: Add minimal password policy

--- web/app/controllers/PublicContentBase.java
+++ web/app/controllers/PublicContentBase.java
@@ -7,11 +7,19 @@ import play.mvc.Controller;
 
 public class PublicContentBase extends Controller {
 
+    public static final int MIN_PASSWORD_LENGTH = 10;
+
     public static void register(){
         render();
     }
 
     public static void processRegister(String username, String password, String passwordCheck, String type){
+        if (!checkPasswordPolicy(password)){
+            flash.error("Password should be at least 10 characters long and bla bla bla");
+            register();
+            return;
+        }
+
         User u = new User(username, HashUtils.getMd5(password), type, -1);
         u.save();
         registerComplete();
@@ -19,5 +27,12 @@ public class PublicContentBase extends Controller {
 
     public static void registerComplete(){
         render();
+    }
+
+    public static boolean checkPasswordPolicy(String password){
+        if (password.length() < MIN_PASSWORD_LENGTH){
+            return false;
+        }
+        return true;
     }
 }
